886e6d4b5199b51dd0f27ec49adb289bc64dc907
hujuc <hujucastro@gmail.com>
2023-11-26 19:40:59 +0000

    ..

--- imageTest.c
+++ imageTest.c
@@ -9,20 +9,22 @@
 
 int main(int argc, char *argv[])
 {
-    ImageInit();
+  ImageInit();
 
-    // criar pgm 1 pixel:
-    Image img2 = ImageCreate(1, 1, 255);
-    // defini-lo todo preto:
-    ImageSetPixel(img2, 0, 0, 0);
+  // criar pgm 1 pixel:
+  Image img2 = ImageCreate(1, 1, 255);
+  // defini-lo todo preto:
+  ImageSetPixel(img2, 0, 0, 0);
 
-    Image img3 = ImageCreate(640, 480, 255);
-    // definir tudo branco
-    ImageSetPixel(img3, 0, 0, 255);
+  Image img3 = ImageCreate(640, 480, 255);
+  // definir tudo branco
+  for (int i = 0; i < 640; i++)
+    for (int j = 0; j < 480; j++)
+      ImageSetPixel(img3, i, j, 255);
 
-    // testar a ImageLocateSubImage com img1 e img2 (worst case)
-    int px, py;
-    InstrReset();
-    ImageLocateSubImage(img3, &px, &py, img2);
-    InstrPrint();
+  // testar a ImageLocateSubImage com img1 e img2 (worst case)
+  int px, py;
+  InstrReset();
+  ImageLocateSubImage(img3, &px, &py, img2);
+  InstrPrint();
 }
